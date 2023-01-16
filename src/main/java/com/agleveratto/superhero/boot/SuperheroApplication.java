4c13b69d5dfd7e12745a9f1b668ac275cc8ea4b6
agleveratto <agleveratto@gmail.com>
2023-01-16 19:04:45 -0300

    feat: add test to validate h2 data and add config to scan entity

--- src/main/java/com/agleveratto/superhero/boot/SuperheroApplication.java
+++ src/main/java/com/agleveratto/superhero/boot/SuperheroApplication.java
@@ -2,9 +2,11 @@ package com.agleveratto.superhero.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.agleveratto.superhero.infrastructure.entities")
 @EnableJpaRepositories(basePackages = "com.agleveratto.superhero.infrastructure.repositories")
 public class SuperheroApplication {
 
--- src/test/java/com/agleveratto/superhero/application/usecases/FindAllSuperheroUseCaseImplTest.java
+++ src/test/java/com/agleveratto/superhero/application/usecases/FindAllSuperheroUseCaseImplTest.java
@@ -1,27 +1,49 @@
 package com.agleveratto.superhero.application.usecases;
 
+import com.agleveratto.superhero.infrastructure.entities.Superhero;
 import com.agleveratto.superhero.infrastructure.repositories.SuperheroRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class FindAllSuperheroUseCaseImplTest {
 
     @InjectMocks
-    FindAllSuperheroUseCaseImpl useCase;
+    private FindAllSuperheroUseCaseImpl useCase;
 
     @Mock
-    SuperheroRepository repository;
+    private SuperheroRepository repository;
+
+    private static Superhero superhero;
+
+    @BeforeAll
+    static void setup(){
+        superhero = new Superhero();
+        superhero.setId(1L);
+        superhero.setName("SUPERMAN");
+    }
 
     @Test
     void execute_whenCallMethod_thenReturnEmptyList(){
         assertThat(useCase.execute()).isNotNull().isEmpty();
         verify(repository).findAll();
     }
+
+    @Test
+    void execute_whenCallMethod_thenReturnSuperheroList(){
+        when(repository.findAll()).thenReturn(List.of(superhero));
+        assertThat(useCase.execute()).isNotNull().containsExactly(superhero);
+        verify(repository).findAll();
+    }
+
 }
