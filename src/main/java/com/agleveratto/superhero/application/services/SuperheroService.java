23a056f397f963965d54e9b5cdbc4cc0d45e8ffc
agleveratto <agleveratto@gmail.com>
2023-01-18 17:27:27 -0300

    fix: add code to solve findAll_whenCallMethod_thenReturnList test

--- src/main/java/com/agleveratto/superhero/application/services/SuperheroService.java
+++ src/main/java/com/agleveratto/superhero/application/services/SuperheroService.java
@@ -1,15 +1,21 @@
 package com.agleveratto.superhero.application.services;
 
+import com.agleveratto.superhero.domain.usecases.FindAllSuperheroUseCase;
 import com.agleveratto.superhero.infrastructure.entities.Superhero;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class SuperheroService {
 
+    private final FindAllSuperheroUseCase findAllSuperheroUseCase;
+
+    public SuperheroService(FindAllSuperheroUseCase findAllSuperheroUseCase) {
+        this.findAllSuperheroUseCase = findAllSuperheroUseCase;
+    }
+
     public List<Superhero> findAll() {
-        return new ArrayList<>();
+        return findAllSuperheroUseCase.execute();
     }
 }
--- src/test/java/com/agleveratto/superhero/application/services/SuperheroServiceTest.java
+++ src/test/java/com/agleveratto/superhero/application/services/SuperheroServiceTest.java
@@ -1,11 +1,19 @@
 package com.agleveratto.superhero.application.services;
 
+import com.agleveratto.superhero.domain.usecases.FindAllSuperheroUseCase;
+import com.agleveratto.superhero.infrastructure.entities.Superhero;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class SuperheroServiceTest {
@@ -13,8 +21,22 @@ public class SuperheroServiceTest {
     @InjectMocks
     SuperheroService superheroService;
 
+    @Mock
+    FindAllSuperheroUseCase findAllSuperheroUseCase;
+
+    private static Superhero superhero;
+
+    @BeforeAll
+    static void setup(){
+        superhero = new Superhero();
+        superhero.setId(1L);
+        superhero.setName("SUPERMAN");
+    }
+
     @Test
     void findAll_whenCallMethod_thenReturnList(){
+        when(findAllSuperheroUseCase.execute()).thenReturn(List.of(superhero));
         assertThat(superheroService.findAll()).isNotEmpty();
+        verify(findAllSuperheroUseCase).execute();
     }
 }
