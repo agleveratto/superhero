994ca1cfe96522d9192009610b9317bdeb5f7ea5
agleveratto <agleveratto@gmail.com>
2023-01-18 18:37:10 -0300

    feat: add main class test

---
+++ src/test/java/com/agleveratto/superhero/boot/SuperheroApplicationTest.java
@@ -0,0 +1,12 @@
+package com.agleveratto.superhero.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SuperheroApplication.class)
+class SuperheroApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
