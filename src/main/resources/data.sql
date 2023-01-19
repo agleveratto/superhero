831fcace51b6ccc01a84132ea1ee39d4b03b9c00
agleveratto <agleveratto@gmail.com>
2023-01-18 21:25:09 -0300

    refactor: add generatedValue annotation and add inserts to db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,34 @@ CREATE TABLE IF NOT EXISTS superhero (
     PRIMARY KEY (ID)
 );
 
-INSERT INTO superhero VALUES (1, 'SUPERMAN')
+INSERT INTO superhero (name) VALUES ('superman');
+INSERT INTO superhero (name) VALUES ('batman');
+INSERT INTO superhero (name) VALUES ('wonderwoman');
+INSERT INTO superhero (name) VALUES ('aquaman');
+INSERT INTO superhero (name) VALUES ('flash');
+INSERT INTO superhero (name) VALUES ('catwoman');
+INSERT INTO superhero (name) VALUES ('green lantern');
+INSERT INTO superhero (name) VALUES ('shazam');
+INSERT INTO superhero (name) VALUES ('black adam');
+INSERT INTO superhero (name) VALUES ('atom smasher');
+INSERT INTO superhero (name) VALUES ('ironman');
+INSERT INTO superhero (name) VALUES ('captain america');
+INSERT INTO superhero (name) VALUES ('spiderman');
+INSERT INTO superhero (name) VALUES ('dr strange');
+INSERT INTO superhero (name) VALUES ('antman');
+INSERT INTO superhero (name) VALUES ('captain marvel');
+INSERT INTO superhero (name) VALUES ('ms marvel');
+INSERT INTO superhero (name) VALUES ('she hulk');
+INSERT INTO superhero (name) VALUES ('hulk');
+INSERT INTO superhero (name) VALUES ('hawkeye');
+INSERT INTO superhero (name) VALUES ('black widow');
+INSERT INTO superhero (name) VALUES ('falcon');
+INSERT INTO superhero (name) VALUES ('scarlet witch');
+INSERT INTO superhero (name) VALUES ('vision');
+INSERT INTO superhero (name) VALUES ('black panther');
+INSERT INTO superhero (name) VALUES ('shang chi');
+INSERT INTO superhero (name) VALUES ('starlord');
+INSERT INTO superhero (name) VALUES ('gamora');
+INSERT INTO superhero (name) VALUES ('mantis');
+INSERT INTO superhero (name) VALUES ('rocket');
+INSERT INTO superhero (name) VALUES ('groot');
