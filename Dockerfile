ebf4ade95cb8f9a930c931268b3191f3faa4a58f
agleveratto <agleveratto@gmail.com>
2023-01-21 20:07:11 -0300

    fix: solve redis issue for connect app to redis

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM amazoncorretto:11-alpine-jdk
-VOLUME /tmp
-EXPOSE 8080
 ARG JAR_FILE=target/superhero.jar
 ADD ${JAR_FILE} app.jar
+EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/agleveratto/superhero/infrastructure/config/RedisConfig.java
+++ src/main/java/com/agleveratto/superhero/infrastructure/config/RedisConfig.java
@@ -5,12 +5,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
@@ -23,25 +20,18 @@ public class RedisConfig {
     private int PORT;
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(){
-        final RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(HOST);
+        config.setPort(PORT);
+        return new JedisConnectionFactory(config);
+    }
 
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(String.class));
-        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
-        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
-
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(HOST, PORT);
-
-        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().build();
-
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisStandaloneConfiguration,
-                jedisClientConfiguration);
-
-        jedisConnectionFactory.afterPropertiesSet();
-
-        redisTemplate.setConnectionFactory(jedisConnectionFactory);
-
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Object.class));
         return redisTemplate;
     }
 }
