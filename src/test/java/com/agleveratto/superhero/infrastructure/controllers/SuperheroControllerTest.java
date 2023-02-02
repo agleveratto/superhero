4114eae7e4eaad8a5cd0b06b94a6971fe8c05675
agleveratto <agleveratto@gmail.com>
2023-02-02 15:18:45 -0300

    test: WIP adding test for SuperheroController

--- src/test/java/com/agleveratto/superhero/infrastructure/controllers/SuperheroControllerTest.java
+++ src/test/java/com/agleveratto/superhero/infrastructure/controllers/SuperheroControllerTest.java
@@ -14,10 +14,12 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(SuperheroController.class)
 public class SuperheroControllerTest {
@@ -47,51 +49,73 @@ public class SuperheroControllerTest {
     @Test
     void findAll_withoutToken_thenReturn401() throws Exception {
         mockMvc.perform(get("/api/v1/"))
-                .andExpect(status().isUnauthorized())
-        ;
+                .andExpect(status().isUnauthorized());
         verify(superheroService, never()).findAll();
+    }
+
+    @Test
+    @WithMockUser(username = "agleveratto@gmail.com")
+    void findAll_withValidMockUser_thenReturn200() throws Exception {
+        when(superheroService.findAll()).thenReturn(List.of(superhero));
+        mockMvc.perform(get("/api/v1/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(superhero.getId()))
+                .andExpect(jsonPath("$[0].name").value(superhero.getName()));
+        verify(superheroService).findAll();
     }
 
     @Test
     void findById_withoutToken_thenReturn401() throws Exception {
         mockMvc.perform(get("/api/v1/{id}", superhero.getId()))
-                .andExpect(status().isUnauthorized())
-        ;
+                .andExpect(status().isUnauthorized());
         verify(superheroService, never()).findById(superhero.getId());
+    }
+
+    @Test
+    @WithMockUser(username = "agleveratto@gmail.com")
+    void findById_withValidMockUser_thenReturn200() throws Exception {
+        when(superheroService.findById(superhero.getId())).thenReturn(superhero);
+        mockMvc.perform(get("/api/v1/{id}", superhero.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(superhero.getId()))
+                .andExpect(jsonPath("$.name").value(superhero.getName()));
+        verify(superheroService).findById(superhero.getId());
     }
 
     @Test
     void findByContains_withoutToken_thenReturn401() throws Exception {
         mockMvc.perform(get("/api/v1/name/{nameContains}", "man"))
-                .andExpect(status().isUnauthorized())
-        ;
+                .andExpect(status().isUnauthorized());
         verify(superheroService, never()).findByContains("man");
     }
 
     @Test
     @WithMockUser(username = "agleveratto@gmail.com")
     void findByContains_withValidMockUser_thenReturn200() throws Exception {
+        when(superheroService.findByContains("man")).thenReturn(List.of(superhero));
         mockMvc.perform(get("/api/v1/name/{nameContains}", "man"))
+                .andDo(print())
                 .andExpect(status().isOk())
-        ;
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(superhero.getId()))
+                .andExpect(jsonPath("$[0].name").value(superhero.getName()));
         verify(superheroService).findByContains("man");
     }
 
     @Test
     void update_withoutToken_thenReturn403() throws Exception {
         mockMvc.perform(put("/api/v1/")
-                        .accept(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(superhero)))
-                .andExpect(status().isForbidden()) // TODO revisar
-        ;
+                .andExpect(status().isForbidden());
         verify(superheroService, never()).update(superhero);
     }
 
     @Test
     void delete_withoutToken_thenReturn403() throws Exception {
         mockMvc.perform(delete("/api/v1/{id}", superhero.getId()))
-                .andExpect(status().isForbidden()) // TODO revisar
-        ;
+                .andExpect(status().isForbidden());
         verify(superheroService, never()).delete(superhero.getId());
     }
 
